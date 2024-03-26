d4f8a6fa8d2725b97ae307070fd053a8d53f05b6
CarlMatthewVerano <113345631+CarlMatthewVerano@users.noreply.github.com>
2024-03-26 22:24:47 +0800

    feat: implemented pseudo functions for CRUD operations
    Co-Authored-By: Hanz Patrick Yu <105765642+dlsyu@users.noreply.github.com>

--- server.js
+++ server.js
@@ -7,26 +7,90 @@ const pool = mysql.createPool(
         host: process.env.MYSQL_HOST,
         database: process.env.MYSQL_DATABASE,
         user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD
+        port: process.env.MYSQL_PORT,
+        // password: process.env.MYSQL_PASSWORD
     }
 ).promise()
 
-export async function getDoctors() {
+// HOW TO CHANGE PORTS DYNAMICALLY
+// function refreshPool() {
+//     pool = mysql.createPool({
+//       host: process.env.MYSQL_HOST,
+//       user: process.env.MYSQL_USER,
+//       database: process.env.MYSQL_DATABASE,
+//       port: process.env.MYSQL_PORT
+//     });
+//   }
+// Call refreshPool() whenever you need to change the port, especially after the first cluster fails.
+
+export async function getNames() {
     // returns an array of objects
-    const [rows] = await pool.query("SELECT * FROM doctors")
+    const [rows] = await pool.query("SELECT * FROM test_table")
     return rows
 }
 
-export async function getDoctor(doctor_id) {
+// export async function getName(name) {
+//     // returns an array of objects
+//     const [rows] = await pool.query(`
+//     SELECT *
+//     FROM test_table
+//     WHERE name = "${name}"
+//     `)
+//     return rows
+// }
+
+const tests = await getNames()
+// gets a single row from the table
+console.log(tests)
+
+//CREATE
+/*
+INSERT INTO tablename (column1, and so on)
+VALUES (value1, and so on);
+export async function insertourmom(param){
+    const [mother'sday] = await pool.query(`
+    INSERT INTO tablename (column1, and so on)
+    VALUES (param.value1, and so on)
+    `)
+}
+*/
+
+//READ
+/*
+SELECT
+FROM
+(whatever, sir oli)``````
+export async function readurmom(param){ ur mum
+    const [mother'sday] = await pool.query(`
+    SELECT  field
+    FROM    table
+    WHERE   condition = "${param}"
+    `)
+
+    return mother'sday
+}
+*/
+
+//UPDATE
+/*
+export async function updater(param) {
     // returns an array of objects
     const [rows] = await pool.query(`
-    SELECT *
-    FROM doctors
-    WHERE doctor_id = "${doctor_id}"
+    UPDATE test_table
+    SET column1 = value1, column2 = value2, ...
+    WHERE param = "${param}";
     `)
     return rows
 }
+ */
 
-const doctors = await getDoctors("1A10A64C1788D23B40B30BF9736B4F57")
-// gets a single row from the table
-console.log(doctors)
+//DELETE
+/*
+export async function deleter(param) {
+    // returns an array of objects
+    const [rows] = await pool.query(`
+    DELETE FROM test_table WHERE param = "${param}";
+    `)
+    return rows
+}
+ */
