d40867852cc2aa720f7fea2f15f1b0610a642b0a
CarlMatthewVerano <113345631+CarlMatthewVerano@users.noreply.github.com>
2024-04-03 19:36:02 +0800

    feat: move to available node in cluster when current is down

--- database.js
+++ database.js
@@ -7,79 +7,123 @@ const app = express();
 
 app.use(express.static('src'));
 
-const pool = mysql.createPool(
+let ports = ['20198', '20199', '20200'];
+let currentPortIndex = 0;
+
+let pool = mysql.createPool(
     {
         host: process.env.MYSQL_HOST,
         database: process.env.MYSQL_DATABASE,
         user: process.env.MYSQL_USER,
-        port: process.env.MYSQL_PORT,
-        // password: process.env.MYSQL_PASSWORD
+        port: ports[currentPortIndex],
     }
 ).promise()
 
-// HOW TO CHANGE PORTS DYNAMICALLY
-// async function executeQuery(query) {
-//     try {
-//       const [rows, fields] = await pool.query(query);
-//       return rows;
-//     } catch (err) {
-//       console.error('Error executing query', err);
-//       if (err.code === 'ETIMEDOUT') {
-//         if (firstFailure) {
-//           process.env.MYSQL_PORT = '20199';
-//           firstFailure = false;
-//         }
-//         refreshPool();
-//         // Retry the query
-//         return executeQuery(query);
-//       } else {
-//         // Handle other errors as necessary
-//         throw err;
-//       }
-//     }
-//   }
-
+function refreshPool() {
+    currentPortIndex = (currentPortIndex + 1) % ports.length;
+    console.log(currentPortIndex)
+    pool = mysql.createPool(
+        {
+            host: process.env.MYSQL_HOST,
+            database: process.env.MYSQL_DATABASE,
+            user: process.env.MYSQL_USER,
+            port: ports[currentPortIndex],
+        }
+    ).promise();
+}
 
 //CREATE (working)
 export async function creator(name, value1) {
-    const [rows] = await pool.query(`
-    INSERT INTO test_table (name, value)
-    VALUES ("${name}", "${value1}")
-    `)
-    return rows
+    try{
+        const [rows] = await pool.query(`
+        INSERT INTO test_table (name, value)
+        VALUES ("${name}", "${value1}")
+        `)
+        return rows
+    } catch (err) {
+        console.error('Error executing query', err);
+        if (err.code === 'ETIMEDOUT') {
+            console.log("Retrying query")
+            refreshPool();
+            // Retry the function
+            return creator(name, value1);
+        } else {
+            // Handle other errors as necessary
+            throw err;
+        }
+    }
 }
 
 //READ (working)
 export async function read(searchTerm) {
-    let sql = "SELECT * FROM test_table";
-    let params = [];
+    try{
+        let sql = "SELECT * FROM test_table";
+        let params = [];
 
-    if (searchTerm) {
-        sql += " WHERE name LIKE ?";
-        params.push(searchTerm);
+        if (searchTerm) {
+            sql += " WHERE name LIKE ?";
+            params.push(searchTerm);
+        }
+        sql += " ORDER BY name";
+        // returns an array of objects
+        const [rows] = await pool.query(sql, params)
+        return rows;
+    } catch (err) {
+        console.error('Error executing query', err);
+        if (err.code === 'ETIMEDOUT') {
+            console.log("Retrying query")
+            refreshPool();
+            // Retry the function
+            return read(searchTerm);
+        } else {
+            // Handle other errors as necessary
+            throw err;
+        }
     }
-    sql += " ORDER BY name";
-    // returns an array of objects
-    const [rows] = await pool.query(sql, params)
-    return rows;
 }
 
 //UPDATE (working)
 export async function updater(param, value1) {
-    // returns an array of objects
-    const [rows] = await pool.query(`
-    UPDATE test_table
-    SET value = "${value1}"
-    WHERE name = "${param}"
-    `)
-    return rows
+    try{
+        // returns an array of objects
+        const [rows] = await pool.query(`
+        UPDATE test_table
+        SET value = "${value1}"
+        WHERE name = "${param}"
+        `)
+        return rows
+    } catch (err) {
+        console.error('Error executing query', err);
+        if (err.code === 'ETIMEDOUT') {
+            console.log("Retrying query")
+            refreshPool();
+            // Retry the function
+            return updater(param, value1);
+        } else {
+            // Handle other errors as necessary
+            throw err;
+        }
+    }
 }
 
 //DELETE (working)
 export async function deleter(param) {
-    // returns an array of objects
-    const [rows] = await pool.query(`
-    DELETE FROM test_table WHERE name = "${param}"
-    `)
-    return rows
+    try{
+        // returns an array of objects
+        const [rows] = await pool.query(`
+        DELETE FROM test_table WHERE name = "${param}"
+        `)
+        return rows
+    } catch (err) {
+        console.error('Error executing query', err);
+        if (err.code === 'ETIMEDOUT') {
+            console.log("Retrying query")
+            refreshPool();
+            // Retry the function
+            return deleter(param);
+        } else {
+            // Handle other errors as necessary
+            throw err;
+        }
+    }
 }
